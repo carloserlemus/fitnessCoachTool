db7b9e8633411904bfd741793cd162ec32207477
carloserlemus <carlosriveralemus@gmail.com>
2020-04-15 19:00:37 -0700

    Added routes/views and template engine.

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const pug = require('pug');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.set('view engine', 'pug')
+app.use(express.static('public'))
+
+// Modules
+const home = require('./routes/home')
+
+app.get('/', home)
+
+app.listen(port, (err)=>{
+    if (err){
+        console.log('Error connecting to port: ', port)
+    } else {
+        console.log('Connected to port: ', port)
+    }
+})
